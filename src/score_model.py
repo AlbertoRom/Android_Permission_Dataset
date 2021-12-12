ab5928cc437cee084e35ab0f0229f825a8e9834a
Alberto <alberto.romeroc@e-campus.uab.cat>
2021-12-12 22:08:51 +0100

    Carpeta "models" feta

--- src/score_model.py
+++ src/score_model.py
@@ -3,15 +3,18 @@ predict_gbc = gbc.predict(X_test)
 cr_gbc = classification_report(y_test, predict_gbc)
 con_mat_gbc = confusion_matrix(y_test, predict_gbc)
 
+
 #Ridge Classifier
 predict_rc = rc.predict(X_test)
 cr_rc = classification_report(y_test, predict_rc)
 con_mat_rc = confusion_matrix(y_test, predict_rc)
 
+
 #Gaussian Naive Bayes
 predict_gnb = gnb.predict(X_test)
 cr_gnb = classification_report(y_test, predict_gnb)
 con_mat_gnb = confusion_matrix(y_test, predict_gnb)
+
 
 #Random Forest Classifier
 predict_rfc = rfc.predict(X_test)
--- src/train_model.py
+++ src/train_model.py
@@ -1,19 +1,24 @@
 #Gradient Boosting Classifier
 gbc = GradientBoostingClassifier()
 gbc.fit(X_train, y_train)
+dump(predict_gbc, '../models/gbc.joblib')
 
 #Ridge Classifier
 rc = RidgeClassifier()
 rc.fit(X_train, y_train)
+dump(predict_gbc, '../models/rc.joblib')
 
 #Gaussian Naive Bayes
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
+dump(predict_gbc, '../models/gnb.joblib')
 
 #Random Forest Classifier
 rfc = RandomForestClassifier()
 rfc.fit(X_train, y_train)
+dump(predict_gbc, '../models/rfc.joblib')
 
 #Regressió Logistica
 lr = LogisticRegression()
 lr.fit(X_train,y_train)
+dump(predict_gbc, '../models/lr.joblib')
